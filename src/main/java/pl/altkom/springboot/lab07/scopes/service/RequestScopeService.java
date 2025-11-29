471ec6dd91d9bd4472a0f1134bdc5ed0c7014bfd
Artur Mroczkowski <a.mroczkowski@intive.com>
2025-11-29 16:20:21 +0100

    Spring Boot 3.5.8 Upgrade

--- src/main/java/pl/altkom/springboot/lab07/scopes/service/RequestScopeService.java
+++ src/main/java/pl/altkom/springboot/lab07/scopes/service/RequestScopeService.java
@@ -1,9 +1,6 @@
 package pl.altkom.springboot.lab07.scopes.service;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.annotation.RequestScope;
 
 //@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
